83782a8ac1a897ff16d5129f0e159eaec134733a
SerGhiniOumaima <serrhinioumayma@gmail.com>
2023-01-02 15:03:12 +0100

    first commit

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,31 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
+		restConfiguration.exposeIdsFor(Customer.class);
+		return args -> {
+			customerRepository.save(new Customer(null,"Ali","ali@gmail.com"));
+			customerRepository.save(new Customer(null,"Rajae","rajae@gmail.com"));
+			customerRepository.save(new Customer(null,"Oumaima","oumaima@gmail.com"));
+			customerRepository.findAll().forEach(customer -> {
+				System.out.println(customer.toString());
+			});
+		};
+	}
+
+}
---
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,30 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		restConfiguration.exposeIdsFor(Product.class);
+		return args -> {
+			productRepository.save(new Product(null,"Ordinateur",778,12));
+			productRepository.save(new Product(null,"Imprimante",88,124));
+			productRepository.save(new Product(null,"Smatphone",58,77));
+			productRepository.findAll().forEach(product -> {
+				System.out.println(product.getName());
+			});
+		};
+	}
+}
